df0fe4df1faca15fb7a02d361659c3a151a0cc8a
Claudio <claudio.raimondi@protonmail.com>
2025-04-25 13:58:09 +0200

    Add NodoVP

---
+++ NodoVP.java
@@ -0,0 +1,20 @@
+/*================================================================================
+
+File: NodoVP.java                                                               
+Creator: Claudio Raimondi                                                       
+Matricola: 7158162                                                              
+
+created at: 2025-04-25 13:58:09                                                 
+last edited: 2025-04-25 13:58:09                                                
+
+================================================================================*/
+
+public class NodoVP<T>
+{
+  public NodoVP(T data)
+  {
+    this.data = data;
+  }
+
+  private T data;
+}
